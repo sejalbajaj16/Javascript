58f0c9705e3921277186b8ee2c3ea17a43fd2ed9
sejalbajaj16 <157449355+sejalbajaj16@users.noreply.github.com>
2024-08-22 06:01:12 +0000

    upload 02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodeJs not browser
+
+console.log(3+3);
+console.log("javascript")
+//code readability should be high
+
