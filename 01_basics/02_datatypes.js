98c4b138085bd907e14ea8a1304831b50d41c996
sejalbajaj16 <157449355+sejalbajaj16@users.noreply.github.com>
2024-08-23 07:37:10 +0000

    upload 02_datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,24 @@ console.log(3+3);
 console.log("javascript")
 //code readability should be high
 
+let name = "sejal" //string data type
+let age = 25 //number data type
+let isLoggedIn = true //boolean data type
+
+//primitive data types
+//number = 2 to power 53
+//bigInt 
+//string = ""
+//boolean = true/false
+//null = standAlone value //empty let age = null
+//undefined = not assigned any value ,  let age
+//symbol = to find uniqueness
+
+
+//object
+
+console.log(typeof age); //number
+console.log(typeof name);//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
